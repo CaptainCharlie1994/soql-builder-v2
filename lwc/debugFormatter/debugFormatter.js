f13c498e42539f80dbc037b9a76cf05437e49473
CaptainCharlie1994 <charlie-herriott@outlook.com>
2025-07-04 10:46:45 +0100

    Removed random root-level LWC folder

--- lwc/debugFormatter/debugFormatter.js
+++
@@ -1,9 +0,0 @@
-export default class debugFormatter {
-  static stringify(obj) {
-    return JSON.stringify(obj, null, 2);
-  }
-
-  static log(label, value) {
-    console.log(`${label}:`, debugFormatter.stringify(value));
-  }
-}
